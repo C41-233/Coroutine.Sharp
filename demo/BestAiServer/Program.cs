011c415f60792393ecb20c42433a319a35a03a3a
C41 <c41hahaha@163.com>
2019-07-24 11:23:22 +0800

    server demo

--- demo/BestAiServer/Program.cs
+++ demo/BestAiServer/Program.cs
@@ -35,6 +35,7 @@ namespace BestAiServer
                 CoroutineManager.StartCoroutine(ProcessClient(client)).Catch(e =>
                 {
                     Console.Error.WriteLine(e);
+                    client.R.Close();
                 });
             }
         }
@@ -48,7 +49,7 @@ namespace BestAiServer
                 yield return WaitFor.Receive(socket, bs).With(out var nread);
                 if (nread.IsError())
                 {
-                    Console.WriteLine($"Error {remote} : {nread.Exception}");
+                    Console.WriteLine($"Error receive {remote} : {nread.Exception}");
                     socket.Close();
                     break;
                 }
@@ -62,7 +63,47 @@ namespace BestAiServer
 
                 var receive = Encoding.UTF8.GetString(bs, 0, nread);
                 Console.WriteLine($"receive {receive} from {socket.RemoteEndPoint}");
+
+                var answer = Answer(receive) + "\n";
+                yield return WaitFor.Send(socket, Encoding.UTF8.GetBytes(answer)).With(out var nsend);
+                if (nsend.IsError())
+                {
+                    Console.WriteLine($"Error send {remote} : {nread.Exception}");
+                    socket.Close();
+                    break;
+                }
+
+                if (nsend <= 0)
+                {
+                    Console.WriteLine($"Close {remote}");
+                    socket.Close();
+                    break;
+                }
+
             }
+        }
+
+        private static string Answer(string question)
+        {
+            question = question.Trim();
+
+            if (question.Contains("什么"))
+            {
+                return "不知道！";
+            }
+
+            question = question.Replace("你", "我");
+            if (question.EndsWith("吗？"))
+            {
+                return question.Substring(0, question.Length - 2) + "！";
+            }
+
+            if (question.EndsWith("？"))
+            {
+                return question.Substring(0, question.Length - 1) + "！";
+            }
+
+            return question;
         }
 
     }
---
+++ project/Coroutine.Sharp/Coroutines/WaitableValue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Coroutines
+{
+    public struct WaitableValue<T> : IWaitable<T>
+    {
+
+        private readonly IWaitable<T> waitable;
+        public T R => waitable.R;
+
+        internal WaitableValue(IWaitable<T> waitable)
+        {
+            this.waitable = waitable;
+        }
+
+        public static implicit operator T(WaitableValue<T> self)
+        {
+            return self.R;
+        }
+
+        public WaitableStatus Status => waitable.Status;
+        public Exception Exception => waitable.Exception;
+
+        public IWaitable Then(Action callback)
+        {
+            return waitable.Then(callback);
+        }
+
+        public IWaitable Catch(Action<Exception> callback)
+        {
+            return waitable.Catch(callback);
+        }
+
+        public void Abort(bool recursive = true)
+        {
+            waitable.Abort(recursive);
+        }
+
+        public IWaitable<T> Then(Action<T> callback)
+        {
+            return waitable.Then(callback);
+        }
+
+        public override string ToString()
+        {
+            if (waitable.Status == WaitableStatus.Success)
+            {
+                return waitable.R?.ToString() ?? "";
+            }
+
+            return "";
+        }
+    }
+}
