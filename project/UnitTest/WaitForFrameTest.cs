5332b2e43889b3c2002bb34e3a9ccd09c17bdbc0
C41 <c41hahaha@163.com>
2019-07-30 11:37:28 +0800

    await unit test

--- project/UnitTest/WaitForFrameTest.cs
+++ project/UnitTest/WaitForFrameTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Coroutines;
+using Coroutines.Await;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest
@@ -55,6 +56,37 @@ namespace UnitTest
                 yield return WaitFor.Yield();
                 i++;
                 yield return WaitFor.Yield();
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public void TestAwait()
+        {
+            var i = 0;
+            var co = CoroutineContainer.StartCoroutine(Run);
+            Assert.AreEqual(0, i);
+            Assert.IsTrue(co.IsRunning());
+
+            CoroutineManager.OneLoop();
+            Assert.AreEqual(1, i);
+            CoroutineManager.OneLoop();
+            Assert.AreEqual(2, i);
+            CoroutineManager.OneLoop();
+            Assert.AreEqual(3, i);
+            CoroutineManager.OneLoop();
+            Assert.AreEqual(3, i);
+            CoroutineManager.OneLoop();
+            Assert.AreEqual(3, i);
+
+            Assert.IsTrue(co.IsSuccess());
+
+            async IWaitable Run()
+            {
+                i++;
+                await WaitFor.Yield();
+                i++;
+                await WaitFor.Yield();
                 i++;
             }
         }
